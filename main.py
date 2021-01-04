e80f383ba9e7a670bbe1cf2358a031559b05bf31
Muhamedhussamm <muhamed_2014@fci.helwan.edu.eg>
2021-01-04 19:24:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from selenium import webdriver
+from selenium import webdriver #@ 34an el program yerun you have to  open chromedriver.exe manually on chrome 87
 from time import sleep
 
-
+#lazm el account yekon m3ml4 follow l7d abl kda 
 class instabot:
     def __init__(self,username,pw):
         self.driver = webdriver.Chrome()
-        self.username =username
         self.driver.get("https://www.instagram.com/")
         sleep(2)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input').send_keys(username)
@@ -15,6 +14,7 @@ class instabot:
         sleep(4)
         self.driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
         sleep(4)
+        #comment next 2 lines lw magt4 saf7et el privacy
         self.driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
         sleep(4)
         self.driver.find_element_by_xpath("//button[contains(text(),'Follow')]").click()
@@ -22,4 +22,4 @@ class instabot:
 
 
 
-instabot('username','pw')
+instabot('username','pw') # write the user name and password manually

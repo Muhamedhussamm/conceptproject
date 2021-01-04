4ec4b815366ce64c8ffe2ad9394063338591670f
Muhamedhussamm <muhamed_2014@fci.helwan.edu.eg>
2021-01-04 20:04:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver #@ 34an el program yerun you have to  open chromedriver.exe manually on chrome 87
 from time import sleep
 
-#lazm el account yekon m3ml4 follow l7d abl kda 
+#lazm el account yekon m3ml4 follow l7d abl kda
 class instabot:
     def __init__(self,username,pw):
         self.driver = webdriver.Chrome()
@@ -17,8 +17,15 @@ class instabot:
         #comment next 2 lines lw magt4 saf7et el privacy
         self.driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
         sleep(4)
-        self.driver.find_element_by_xpath("//button[contains(text(),'Follow')]").click()
-        sleep(4)
+                for i in range(2):
+            for i in range(5):
+                self.driver.find_element_by_xpath(("//button[text()='Follow']")) \
+                    .click()
+
+                sleep(2)
+
+
+        sleep(2)
 
 
 
